1c067a1d423e4f5b27783ac2a494af7ff85a2614
hnrqss <hnrq.assuncao@gmail.com>
2019-10-29 20:46:45 -0300

    main style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './utils/api'
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        
+      </div>
+    </>
   );
 }
 
